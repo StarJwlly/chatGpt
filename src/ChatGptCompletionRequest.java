1e64f3c167a9bdc2f11820955835b370c9afc572
Aluno <aluno@IPIRANGA.FATEC>
2023-05-18 17:35:24 -0300

    chatgpt modelos lombok gson testegson

---
+++ src/ChatGptCompletionRequest.java
@@ -0,0 +1,14 @@
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ChatGptCompletionRequest {
+    private String model;
+    private String prompt;
+    private int max_tokens;
+}
---
+++ src/TesteGson.java
@@ -0,0 +1,15 @@
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+public class TesteGson {
+    public static void main(String[] args) {
+        ChatGptCompletionRequest request = new ChatGptCompletionRequest(
+            "text-davinci-003", "Por que o ceu é verde?", 150);
+        
+        Gson gson = new Gson();
+        String json = gson.toJson(request);
+        System.out.println(json);
+
+        
+    }
+}
